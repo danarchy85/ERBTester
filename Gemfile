165820321f19e25cc2575ddd4f1afac5bf22d14d
Dan James <dan@danarchy.me>
2024-09-19 10:51:56 -0700

    create ERBTestergemspec and Ruby structure

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in ERBTester.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/ERBTester.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ERBTester/version"
+
+module ERBTester
+  class Error < StandardError; end
+  # Your code goes here...
+end
